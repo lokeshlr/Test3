d361f9cc3597fb7029911877d9e30595c4a79209
LOKESH L R <lokesh.lr@sonata-software.com>
2016-07-05 17:36:02 +0530

    updated the groovy file

--- WebAppAutomationTest/test.groovy
+++ WebAppAutomationTest/test.groovy
@@ -1,5 +1,16 @@
-job('test31'){
-steps{
-batchFile(readFileFromWorkspace('test.bat'))
-}
+def jobName_1=%JOB_NAME%
+def gitRepoPath='lokeshlr/Test3'
+def gitBranchName='*/master'
+job(jobName_1){
+description(jobName_1)
+scm{
+     github(gitRepoPath,gitBranchName)
+    }
+  steps{
+    batchFile('cd %WORKSPACE%\\WebAppAutomationTest'+'\n'+'test.bat')
+  }
+  triggers {
+        scm('*/5 * * * *')
+        githubPush()
+    }
 }
