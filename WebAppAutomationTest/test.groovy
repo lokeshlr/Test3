a63cdbbb979afb36c84184edb85829f73774106a
lokeshlr <lokesh1990.stars@gmail.com>
2016-07-07 15:00:36 +0530

    Update test.groovy

--- WebAppAutomationTest/test.groovy
+++ WebAppAutomationTest/test.groovy
@@ -1,23 +1,27 @@
-def jobName_1 = 'Test1'
-def gitRepoPath = 'lokeshlr/Test3'
-def gitBranchName = '*/master'
+/** Below  are the parameters need to define by user*/ 
+def JOB_NAME = 'Test1'
+def GIT_REPO_PATH = 'lokeshlr/Test3'
+def GIT_BRANCH_NAME = '*/master'
 def ARTIFACT_SOURCE_PATH = '**/test-output/index.html'
 def S3_PROFILE_NAME = 'pennymacadmin'
 def S3_BUCKET_NAME = 'pnmac-jenkins-archive'
 def S3_STORAGE_CLASS_NAME = 'STANDARD'
 def S3_STORAGE_REGION = 'us-east-1'
+def GROOVY_SCRIPT_PATH = '**/test.groovy'
+def BATCH_FILE_PATH = 'cd %WORKSPACE%\\WebAppAutomationTest'+'\n'+'test.bat'
+def SCM_TRIGGER = '*/5 * * * *'
 
-freeStyleJob(jobName_1){
-description(jobName_1)
+freeStyleJob(JOB_NAME){
+description(JOB_NAME)
 scm{
-     github(gitRepoPath,gitBranchName)
+     github(GIT_REPO_PATH,GIT_BRANCH_NAME)
     }
   steps{
-    batchFile('cd %WORKSPACE%\\WebAppAutomationTest'+'\n'+'test.bat')
-    dsl{external('**/test.groovy')}
+    batchFile(BATCH_FILE_PATH)
+    dsl{external(GROOVY_SCRIPT_PATH)}
   }
   triggers {
-        scm('*/5 * * * *')
+        scm(SCM_TRIGGER)
         githubPush()
     }
 	
